3f41a45aa258061bc42586b114ecf24338c8b37b
maralgharibi <maral.gharibi1383@gmail.com>
2024-11-01 15:15:42 +0330

    new

--- users/urls.py
+++ users/urls.py
@@ -1,14 +1,14 @@
-
-from django.contrib import admin
 from django.urls import path
+
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+
 from .views import RegisterView, GetTokenView
+
 
 urlpatterns = [
     path('register/', RegisterView.as_view()),
-    path('users/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('users/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('get-token/', GetTokenView.as_view()),
 ]
--- users/views.py
+++ users/views.py
@@ -8,16 +8,16 @@ from rest_framework.response import Response
 from rest_framework import status
 
 from .models import User
-
 class RegisterView(APIView):
+
     def post(self, request):
-        phone_number =request.date.get('phone_number')
+        phone_number = request.data.get('phone_number')
+
         if not phone_number:
             return Response(status=status.HTTP_400_BAD_REQUEST)
 
         try:
             user = User.objects.get(phone_number=phone_number)
-
         except User.DoesNotExist:
             user = User.objects.create_user(phone_number=phone_number)
 
@@ -27,6 +27,7 @@ class RegisterView(APIView):
         cache.set(str(phone_number), code, 2 * 60)
 
         return Response({'code': code})
+
 
 class GetTokenView(APIView):
 
